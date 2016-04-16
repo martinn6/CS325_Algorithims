c2a6fe19a731eae4c25c91ab4beceaae57283a53
nickdmartin <nickdmartin@outlook.com>
2016-04-16 14:27:40 -0700

    Added test text files

--- Project01/Makefile.mak
+++ Project01/Makefile.mak
@@ -1,9 +1,9 @@
 CFLAGS = -Wall -fpic -coverage -lm -std=c99
 
 project01: Project01.c
-	gcc -o Project01 Project01.c $(CFLAGS)
+	gcc -o project01 Project01.c $(CFLAGS)
 
 all: project01
 
 clean:
-	rm -f *.o Project01.exe
+	rm -f *.o project01.exe
--- Project01/Project01.c
+++ Project01/Project01.c
@@ -22,7 +22,40 @@ int enumeration1(int n)
 
 main()
 {
+	//Declare Variables
+	file thisfile;
+	bool fileLoaded = 0;
+	string inputFileName;
+	string outputFileName;
+	
 	//Load file
+	while(fileLoaded == 0)
+	{
+		//Get User Input
+		cout << "\nEnter the name for the input file: ";
+		getline(cin, inputFileName);
+						
+		//OPEN FILE 
+		ifstream inputFile(inputFileName.c_str());
+		//ERROR HANDELING
+		if (inputFile)
+		{
+			cout << "\nOpening " << inputFileName << "...";
+			fileLoaded=1;
+			inputFile.close();
+		}
+		else
+		{
+			cout << "\nError 404: File " << inputFileName << " not found!";
+		}	
+	}
+	
+	//open inputfile	
+	ifstream inputFile(inputFileName.c_str());
+	
+	
+	
+	
 	//Run Test 1
 	enumeration1(0);
 	//Run Test 2
