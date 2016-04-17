c3d266d4d549ad89db917d729e45f5925983cd1e
nickdmartin <nickdmartin@outlook.com>
2016-04-17 16:32:38 -0700

    test

--- Project01/Project01.c
+++ Project01/Project01.c
@@ -76,10 +76,9 @@ int algoThreeMaxSubArray(struct lines *line, int lineNum, int a[], int lo, int h
 				maxFirst = sum;
 				//get sub-array
 				line[lineNum].subArrayLength = 0; //reset array back to zero position
-				//int n = 0;
 				for (int b = i; b >= 0; b--)
 				{
-					//line[lineNum].subArray[line[lineNum].subArrayLength] = a[b]; //subArray[position] = a[b]
+					line[lineNum].subArray[line[lineNum].subArrayLength] = a[b]; //subArray[position] = a[b]
 					line[lineNum].subArrayLength++; //increaes subArray length
 				}
 				//printf("maxFirst = %d", maxFirst);
