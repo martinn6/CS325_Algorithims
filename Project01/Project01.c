6e48ababf29d554dd507b4f7d90609bfd9a5fbaa
nickdmartin <nickdmartin@outlook.com>
2016-04-17 22:28:38 -0700

    fix

--- Project01/Project01.c
+++ Project01/Project01.c
@@ -408,7 +408,7 @@ int main()
 	{
 		//Run Test 4
 		line[0].noOfLines = 1;
-		int result = algoFourLinearTime(&line, line[0].num, line[0].noOfnums); 
+		algoFourLinearTime(&line, line[0].num, line[0].noOfnums); 
 		outputResults(&line);
 		outputResultsToFile(&line);
 	}
