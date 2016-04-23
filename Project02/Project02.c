872692169e1f317d51b52227b3d40a508e392864
nickdmartin <nickdmartin@outlook.com>
2016-04-23 14:57:21 -0700

    Latest production version. Working algorithim.

--- Project02/Project02.c
+++ Project02/Project02.c
@@ -1,10 +1,8 @@
 /*********************************************************************************************
  * Developers: Nick Martin (martinn6)
-	Elizabeth 
-
  * Date: 20160422
  * Project: Project 02
- * Description: Fibonacci recusive program
+ * Description: Implement a greedy algorithm.
  * *******************************************************************************************/
 
 #include <math.h>
@@ -14,106 +12,133 @@
 #include <time.h>
 #include <unistd.h>
 
-/**********************************************************************\
-This will take in an int array and output time.
-\***********************************************************************/
-
-int Greedy(int v[], int c[], int a, int m)
+/**********************************************************
+Name: randomNum()
+Description: Used for testing the greedy function. It will
+	populate the array V with random coin values to 
+	maxNumOfCoins with the larget coin size of  maxCoinValue.
+	It will also calculate a random maxValue.
+**********************************************************/
+int randomNum(int *v, int *a, int maxNumOfCoins, int maxCoinValue, int maxValue)
 {
+	int temp;
+	
+	//random select value
+	*a = rand() % (maxValue-1) + 1;
+	
+	//fill coins (make sure not to have two coins with same value)
+	int x = 0, numFound;
+	while(x < maxNumOfCoins)
+	{
+		numFound = 0;
+		temp = rand() % (maxCoinValue-1) + 1;
+		for(int y = 0; y < maxNumOfCoins; y++)
+		{
+			if(temp == v[y])
+				numFound = 1;
+		}
+		if (numFound == 0)
+		{
+			v[x] = temp;
+			x++;
+		}
+	}	
+	
+	//sort coins in accending order (bubble sort)
+	 for (int i = 0; i < maxNumOfCoins; i++) {
+        for (int j = 0; j < maxNumOfCoins-i - 1; j++) {
+            if (v[j] > v[j + 1]) {
+                int temp = v[j];
+                v[j] = v[j + 1];
+                v[j + 1] = temp;
+            }
+        }
+    }
+	
+	return(0);
+}
+
+/**********************************************************
+Name: runGreedyAlgorithm()
+Description: Finds the least amount of coins needed by 
+	starting with the largest coin value and progressing
+	to the smallest coin value. It will calculate:
+		1) # of each coin used
+		2) Total # of coins used.
+**********************************************************/
+int runGreedyAlgorithm(int v[], int a, int *c, int *m, int length)
+{
+	int value = a,
+		tempValue = 0,
+		n = 1;
+	
+	*m = 0; //Set # of coins to zero
+		
+	while(value > 0 && (length - n) >= 0)
+	{
+		slowDown();	//slowing down the function so I can track its time.
+		tempValue = value - v[length-n];
+		if (tempValue >= 0)
+		{		
+			value = tempValue; 
+			c[length-n] = c[length-n]+1; 				//increment count array for coin used
+			*m = *m+1;									//increment number of coins used.
+			/* FOR TESTING ONLY
+			printf("Coin Used = %d\n", v[length-n]);	
+			printf("New Value: %d\n", value);
+			printf("Total Coins Used: %d\n", *m);
+			*/
+			
+		}
+		else
+		{
+			n++;
+		}
+	}
+	
+	/*print out c test
+	for(int x = 0; x < length; x++)
+	{
+		printf("c[%d] = %d; ", x, c[x]);
+	}
+	*/
 	
 	return 0;
 }
 
-size_t strlen(const char *str)
+int slowDown()
 {
-    const char *s;
-    for (s = str; *s; ++s);
-    return(s - str);
-}
-
-int outputResults(struct lines *line)
-{
-	//Test Output
-	/*
-	printf("Results:\n\n");
-	for (int i = 0; i < line[0].noOfLines; i++)
-	{
-		printf("[");
-		for (int j = 0; j < line[i].noOfnums; j++)
-		{
-			printf("%i", line[i].num[j]);
-			if(j != line[i].noOfnums - 1)
-				printf(", ");
-		}
-		printf("]\n");
-		printf("[");
-		for (int j = 0; j < line[i].subArrayLength; j++)
-		{
-			printf("%i", line[i].subArray[j]);
-			if(j != line[i].subArrayLength - 1)
-				printf(", ");
-		}
-		printf("]\n");
-		printf("max sum = %d\n", line[i].maxArraySum);
-	}
-	*/
-	return 0;
-}
-
-
-int outputResultsToFile(struct lines *line)
-{
-	//Test Output
-	/*
-	FILE *fp;
-	const char* outfilename;
-	outfilename = "MSS_Results.txt";
-	fp = fopen(outfilename, "w");
-	//printf("Results:\n\n");
-	for (int i = 0; i < line[0].noOfLines; i++)
-	{
-		fprintf(fp,"%s","[");
-		for (int j = 0; j < line[i].noOfnums; j++)
-		{
-			fprintf(fp,"%i", line[i].num[j]);
-			if(j != line[i].noOfnums - 1)
-				fprintf(fp,"%s",", ");
-		}
-		fprintf(fp,"%s","]\n");
-		fprintf(fp,"%s","[");
-		for (int j = 0; j < line[i].subArrayLength; j++)
-		{
-			fprintf(fp,"%i", line[i].subArray[j]);
-			if(j != line[i].subArrayLength - 1)
-				fprintf(fp,"%s",", ");
-		}
-		fprintf(fp,"%s","]\n");
-		fprintf(fp,"%s %d %s","max sum = ", line[i].maxArraySum,"\n");
-	}
-	printf("Output results to file: %s", outfilename);
-	//close file
-	fclose(fp);
-	*/
-	return 0;
-}
-
-int randomNum(int *v, int a, int maxNumOfCoins, int maxValue)
-{
-	//fill coins
-	for(int x = 0; x < maxNumOfCoins; x++)
-	{
-		v[x] = rand() % maxValue + 1;
-	}
+	int n = 0;
 	
-	for(int x = 0; x < maxNumOfCoins; x++)
+	for(int x=0; x< 1000000; x++)
+		n = x;
+}
+
+/**********************************************************
+Name: outputResults()
+Description: outputs the results to screen in the format:
+	[ArrayName] = [n0, n1, ... n[Array.Length]]
+**********************************************************/
+int outputResults(int c[], int length, int m, char arrayName)
+{
+	//Test Output
+	printf("%c=[", arrayName);
+	for (int x = 0; x < length; x++)
 	{
-		printf("v[%d]=%d",x,v[x]);
+		printf("%d", c[x]);
+		if(x != length-1)
+			printf(", ");
 	}
+	printf("]\n");
+	printf("%d\n", m);
+
+	return 0;
 }
 
 
 int main()
 {
+	srand(time(NULL));
 	//Declare Variables
 	FILE *fp;
 	char *line = NULL;
@@ -121,21 +146,36 @@ int main()
     ssize_t read;
 	char filename[99];
 	char *numLine;
-	char *userInput;
+	char *userInput = (char*) malloc(100);
 	char *valueLine;
 	char buffer[100];
 	int n = 0;
-	int maxValue = 1000;
+	clock_t start, end;
+	float elapsed_time;
+
+	//For random generation
+	int maxValue = 10000;
 	int maxNumOfCoins = 10;
+	int maxCoinValue = 50;
+	
+	//regular variables
 	int v[maxNumOfCoins];
 	int c[maxNumOfCoins];
-	int a;	//start value
+	int a, m;	//start value
+	
+	//prep arrays
+	for(int x = 0; x < maxNumOfCoins; x++)
+	{
+		c[x] = 0;
+		v[x] = 0;
+	}
 
 	//ask user for file or random generator
 	printf("What do you want to run:\n");
 	printf("1) Load from File\n");
 	printf("2) Random Generator\n");
 	
+	//Get user input
 	scanf ("%[^\n]%*c", userInput);
 	
 	if (strcmp(userInput,"1") == 0)
@@ -144,45 +184,58 @@ int main()
 	}
 	else if (strcmp(userInput,"2") == 0)
 	{
-		//Run Random Generator\n
-		randomNum(&v, a, maxNumOfCoins,  maxValue);
-		
+		//Run Random Generator
+		randomNum(&v, &a, maxNumOfCoins, maxCoinValue, maxValue);
 	}
 	
-	printf("Enter filename: ");
-	fgets(filename, 99, stdin);
-	char *p = strchr(filename, '\n'); // p will point to the newline in filename
-	if(p) *p = 0; // if p is not null, terminate filename at p
+	//run Greedy
+	start = clock();
+	runGreedyAlgorithm(v, a, &c, &m, maxNumOfCoins);
+	end = clock();
+	elapsed_time = (float)(end - start) / (1.0*CLOCKS_PER_SEC);
 	
-	fp = fopen(filename, "r");
+	printf("\nmaxNumOfCoins=%d\n", maxNumOfCoins);
+	printf("maxCoinValue=%d\n", maxCoinValue);
+	printf("maxValue=%d\n", maxValue);
+	outputResults(v, maxNumOfCoins, a, 'V');
+	printf("Elapsed time: %f seconds\n", elapsed_time);
+	printf("\nResults:\n");
+	outputResults(c, maxNumOfCoins, m, 'C');
+
+	// printf("Enter filename: ");
+	// fgets(filename, 99, stdin);
+	// char *p = strchr(filename, '\n'); // p will point to the newline in filename
+	// if(p) *p = 0; // if p is not null, terminate filename at p
 	
-	if(!fp)
-		perror("File not found");
-	else
-	{
-		printf("opening file: %s...\n", filename);
-		while ((read = getline(&line, &len, fp)) != -1) {
-			if (n == 1)
-			{
-				numLine = (char *) realloc (numLine, strlen(line)+1);
-				strncpy(numLine, line, strlen(line));
-				*p = strchr(numLine, '\n'); // p will point to the newline in filename
-				if(p) *p = 0; // if p is not null, terminate filename at p
+	// fp = fopen(filename, "r");
+	
+	// if(!fp)
+		// perror("File not found");
+	// else
+	// {
+		// printf("opening file: %s...\n", filename);
+		// while ((read = getline(&line, &len, fp)) != -1) {
+			// if (n == 1)
+			// {
+				// numLine = (char *) realloc (numLine, strlen(line)+1);
+				// strncpy(numLine, line, strlen(line));
+				// *p = strchr(numLine, '\n'); // p will point to the newline in filename
+				// if(p) *p = 0; // if p is not null, terminate filename at p
 				
-			}
-			if (n == 2)
-			{
-				printf("n=2\n");
-				printf("strlen=%d\n",strlen(line));
-				valueLine = (char *) realloc (valueLine, strlen(line)+1);
-				// strncpy(valueLine, line, strlen(line));
-			}
-			n++;
-		}
-    }
+			// }
+			// if (n == 2)
+			// {
+				// printf("n=2\n");
+				// printf("strlen=%d\n",strlen(line));
+				// valueLine = (char *) realloc (valueLine, strlen(line)+1);
+				//strncpy(valueLine, line, strlen(line));
+			// }
+			// n++;
+		// }
+    // }
  
-	printf("numLine=%s\n", numLine);
-	printf("valueLine=%s\n", valueLine);
+	// printf("numLine=%s\n", numLine);
+	// printf("valueLine=%s\n", valueLine);
 
  
 	/*
@@ -203,87 +256,8 @@ int main()
 	}
 	*/
 	//close file
-	fclose(fp);
+	//fclose(fp);
 	
-	/* random number generator for tests
-
-	int len = 1000;
-	int testArr[10][len];
-	for(int i = 0; i < 10; i++) {
-		for (int j = 0; j < len; j++) {
-			testArr[i][j] = rand();
-		}
-	}
-	
-	for (int n = 0; n < 10; n++)
-	{
-		for (int i = 0; i < len; i++)
-		{
-			line[n].num[i] = testArr[n][i];
-		}
-	}
-	
-	clock_t start, end;
-	float elapsed_time;
-	start = clock();
-	for(int x = 0; x< 100000; x++)
-		x = x;
-	end = clock();
-	elapsed_time = (float)(end - start) / (float)CLOCKS_PER_SEC;
-	printf("Elapsed time: %f seconds\n", elapsed_time);
-	
-
-	for (int i = 0; i < 9; i++){
-
-		algoOneMaxSubarray(&line); 
-
-	}	
-    */
-	 
-	//ask user for which algorithm to run
-	/*
-	printf("Choose which algorithim to run:\n");
-	printf("1) Algorithm 1: Enumeration\n");
-	printf("2) Algorithm 2: Better Enumeration\n");
-	printf("3) Algorithm 3: Divide and Conquer\n");
-	printf("4) Algorithm 4: Linear-time\n");
-	
-	scanf ("%[^\n]%*c", userInput);
-	
-	if (strcmp(userInput,"1") == 0)
-	{
-		//Run First Algorithim
-		algoOneMaxSubarray(&line);
-		outputResults(&line);
-		outputResultsToFile(&line);
-	}
-	else if (strcmp(userInput,"2") == 0)
-	{
-		//Run Test 2
-		algoTwoMaxSubarray(&line);
-		outputResults(&line);
-		outputResultsToFile(&line);
-	}
-	else if (strcmp(userInput,"3") == 0)
-	{
-		int a = 0;
-		algoThreeMaxSubArray(&line, a, line[a].num, 0, line[a].noOfnums); //struct, lineNum, numArray, lo, hi
-		outputResults(&line);
-		outputResultsToFile(&line);
-	}
-	else if (strcmp(userInput,"4") == 0)
-	{
-		//Run Test 4
-		line[0].noOfLines = 1;
-		algoFourLinearTime(&line, line[0].num, line[0].noOfnums); 
-		outputResults(&line);
-		outputResultsToFile(&line);
-	}
-	else
-	{
-		printf("Invalid Input.");
-	}
-	*/
 	//Exit
 	printf("\nHave a nice day.\n");
 	return 0;
